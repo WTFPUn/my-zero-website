217177ccbf466ae8953938daa39d5add8273783d
WTFPUn <punn_namw@hotmail.com>
2021-07-30 02:57:12 +0700

    fix: image size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,8 @@ export default function Navbar(){
 
 
     return (
-        <div className="bg-yellow-300 flex flex-row w-full">
-        <div className="px-3"><Image className="flex m-2 p-5 w-24 h-24 border-r-2 items-center" src={logoimg}/></div>
+        <div className="bg-yellow-300 flex flex-row w-full items-center">
+        <div className="px-3"><div className="relative flex m-2 p-5 w-12 h-12 border-r-2 items-center"><Image layout="fill" src={logoimg}/></div></div>
         <div className="flex p-6 pl-0.5 mr-12 font-medium text-xl">
           Reserve <br />System
         </div>
