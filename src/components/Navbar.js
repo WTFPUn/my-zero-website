c00a6fe10b8ad46b0afc375f4589e3654159fbfc
WTFPUn <punn_namw@hotmail.com>
2021-07-30 19:58:07 +0700

    change  navigator bar color (forget lol)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,40 +8,13 @@ export default function Navbar(){
 
 
     return (
-        <div className="bg-yellow-300 flex flex-row w-full items-center">
+        <div className="bg-blue-700 flex flex-row w-full items-center">
         <div className="px-3"><div className="relative flex m-2 p-5 w-12 h-12 border-r-2 items-center"><Image layout="fill" src={logoimg}/></div></div>
         <div className="flex p-6 pl-0.5 mr-12 font-medium text-xl">
           Reserve <br />System
         </div>
         <div className="p-6 pt-10 ml-10">จองห้องประชุม</div>
-        <div >
-        <Menu as = "div" className="relative items-center" >
-          <Menu.Button 
-            onMouseEnter={() => setIsMenuHovered(true)}
-            onMouseLeave={() => setIsMenuHovered(false)}
-          >
-            <div className=" flex pt-10 p-12 ml-10 items-end  bg-gradient-to-b hover:from-white  hover:to-yellow-300">การจองของคุณ</div>
-          </Menu.Button>
-
-          <Transition
-            show={isMenuHovered}
-            enter="transition duration-200 ease-in-out"
-            enterFrom="transform -translate-y-2 scale-0"
-            enterTo="transform scale-100"
-            leave="transition duration-200 ease-in-out"
-            leaveFrom="transform scale-100"
-            leaveTo="transform -translate-y-2 scale-0"
-          >
-            <Menu.Items as="div" className="absolute  right-15 transform -translate-y-2  items-center w-full rounded-xl bg-white divide-y divide-light-yellow-400 mt-5 ">
-              <Menu.Item><div >eiei<br/>sdsdsd<br/></div></Menu.Item>
-              <Menu.Item><div>eiei1</div></Menu.Item>
-              <Menu.Item><div>eiei1</div></Menu.Item>
-              <Menu.Item><div>eiei1</div></Menu.Item>
-              <Menu.Item><div>eiei1</div></Menu.Item>
-            </Menu.Items>
-          </Transition>
-        </Menu>
-        </div>
+        
         <div className="p-6 m-5 ml-auto border-r-2">Sign in</div>
         <div className="p-6 mr-5 mt-5 mb-5">Sign up</div>
 
