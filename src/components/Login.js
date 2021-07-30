0571da6f8c46a7d0cbf204079dad9d340f39a6d6
WTFPUn <punn_namw@hotmail.com>
2021-07-31 03:20:28 +0700

    ▫missing word

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,7 +22,7 @@ export default function Login() {
           type="button"
           onClick={openModal}
           className="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-             Open dialog
+             Sign Up
             </button>
     <Transition appear show={isOpen} as={Fragment}>
         <Dialog
