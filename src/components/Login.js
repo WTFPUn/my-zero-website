2772e3e0071b0333716e2970597d34bf19d95427
WTFPUn <punn_namw@hotmail.com>
2021-07-31 16:36:20 +0700

    add:Sign up  modal detail

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,12 @@
-import { useEffect, useState,Fragment } from "react"
-import { Transition,Dialog } from "@headlessui/react"
+import { useEffect, useState, Fragment } from "react"
+import { Transition, Dialog } from "@headlessui/react"
 
 
 
 export default function Login() {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
+  const [signupform, setsignupform] = useState([{ username: '', password: '', confirmpassword: '' }])
+
 
   function closeModal() {
     setIsOpen(false)
@@ -14,17 +16,17 @@ export default function Login() {
     setIsOpen(true)
   }
 
-    return(
-        
-            
-        <>
-        <button
-          type="button"
-          onClick={openModal}
-          className="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-             Sign Up
-            </button>
-    <Transition appear show={isOpen} as={Fragment}>
+  return (
+
+
+    <>
+      <button
+        type="button"
+        onClick={openModal}
+        className="px-4 mr-5 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+        Sign Up
+      </button>
+      <Transition appear show={isOpen} as={Fragment}>
         <Dialog
           as="div"
           className="fixed inset-0 z-10 overflow-y-auto"
@@ -43,7 +45,7 @@ export default function Login() {
               <Dialog.Overlay className="fixed inset-0" />
             </Transition.Child>
 
-            {/* This element is to trick the browser into centering the modal contents. */}
+
             <span
               className="inline-block h-screen align-middle"
               aria-hidden="true"
@@ -59,34 +61,64 @@ export default function Login() {
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+              <div className="inline-block w-full max-w-md  p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
                 <Dialog.Title
-                  as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900"
+                  as="h1"
+                  className="text-lg font-medium  leading-6 text-gray-900"
                 >
-                  Sign up
+                  Create your account
                 </Dialog.Title>
                 <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    work Tomorrow Naja
-                  </p>
+                  <form className="text-sm text-gray-500">
+                    <div className="flex flex-row">
+                      <div className="flex flex-col">
+                        <div>Your name</div>
+                        <input className="border-2 " placeholder="Johny" />
+                      </div>
+                      <div className="flex flex-col ml-5">
+                        <div>Last name</div>
+                        <input className="border-2" placeholder="Eieiderka" />
+                      </div>
+                    </div>
+                    <div className="flex flex-col">
+                      <div className="flex flex-col">
+                        <div>Username</div>
+                        <input className="border-2" placeholder="it up to you!" />
+                      </div>
+                      <div>E-mail:</div>
+                        <input className="border-2" placeholder="example@email.com" />
+                      
+                    </div>
+                    
+                    <div>
+                      <div className="flex flex-row">
+                        <div className="flex flex-col">
+                          <div>password</div>
+                          <input className="border-2 w-40" type="password" placeholder="contain more than 8 characters" />
+                        </div>
+                        <div className="flex flex-col ml-5">
+                          <div>conform password</div>
+                          <input className="border-2" type="password" placeholder="conform password" />
+                        </div>
+                      </div>
+                      </div>
+                  </form>
                 </div>
 
-                <div className="mt-4">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                    onClick={closeModal}
-                  >
-                    Okayๆๆ
-                  </button>
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                      onClick={closeModal}
+                      >
+                    </button>
+                        Register
+                  </div>
                 </div>
-              </div>
             </Transition.Child>
           </div>
         </Dialog>
       </Transition>
-      </>
-    )
-  }
-  
+    </>
+      )
+}
